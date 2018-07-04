874b2b0ab9eead03a35f0915f5b3ff1f8bc8f7c5
Pushkar Dharmatti <pushkar.dharmatti@gmail.com>
2018-07-04 15:57:44 -0400

    Initial commit

---
+++ mylogparse.py
@@ -0,0 +1,26 @@
+import csv
+import pandas as pd
+
+inputcsv = open('C:\\Users\Castiel\Desktop\inputcsv.csv', 'w', encoding = 'utf-8')
+with open("C:\\Users\Castiel\Desktop\inputs.txt", 'r') as inputfile:
+    input = inputfile.readlines()
+    #print(input)
+    list = []
+    for line in input:
+        listline= line.replace('\n\n','\n').split()
+        if not line.isspace():
+            list.append(line.split())
+    for eachval in list:
+        csvline = ','.join(eachval)
+        inputcsv.write(csvline + '\n')
+inputfile.close()
+inputcsv.close()
+df = pd.read_csv('C:\\Users\Castiel\Desktop\inputcsv.csv')
+df2 = df.groupby(['Date', 'IP', 'State']).size()
+df2.to_csv('C:\\Users\Castiel\Desktop\output.csv', header=["Count"])
+
+    
+
+
+
+
